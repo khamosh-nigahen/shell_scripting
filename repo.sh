48e2fd74d208553600c9b57df21fb1a30663ef2f
Puneet Jain <mailpuneet8264@gmail.com>
2017-02-03 18:20:26 +0530

    Just a simple start

---
+++ repo.sh
@@ -0,0 +1,6 @@
+bash
+sleep 1s
+#cd /git/gitusers/qjv867/WAVE7K_SERVER/wave7kc-server/
+#alias repo="cd /git/gitusers/qjv867/WAVE7K_SERVER/wave7kc-server/"
+#repo
+`cd /git/gitusers/qjv867/WAVE7K_SERVER/wave7kc-server/`
